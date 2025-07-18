e03a922b98a5180ffdcd17f75537e1394ab0d28f
Leandro Farias <leandrorf1606@gmail.com>
2025-07-18 19:03:48 -0300

    feat: CORS

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ import publicRouter from "./routes/publicRoutes.js"
 import privateRouter from "./routes/privateRoutes.js"
 import cookieParser from "cookie-parser"
 
-const PORT = 3333
+const allowedOrigins = ["http://localhost:3000",]
+
+const PORT = process.env.PORT || 3333
 const app = express() 
 app.use(express.json())
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser())
 
 app.use(publicRouter)
 app.use(privateRouter)
 
-app.listen(PORT, console.log(`Server rodando na porta ${PORT}`))
+app.listen(PORT, console.log(`Servidor rodando na porta: ${PORT}`))
