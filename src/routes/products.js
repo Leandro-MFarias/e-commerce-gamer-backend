0c99495c37d6c5aa02de3efb8d09aeed4617018d
Leandro Farias <leandrorf1606@gmail.com>
2025-09-19 11:26:15 -0300

    select categories form products route

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,11 +4,20 @@ const prisma = new PrismaClient()
 
 export async function products(req, res) {
   try {
-    const products = await prisma.product.findMany()
-    if (!products) return 
+    const products = await prisma.product.findMany({
+      include: {
+        categories: {
+          select: {
+            id: true,
+            name: true,
+          }
+        }
+      }
+    })
+    if (!products) return res.status(404).json({ message: "Nenhuma produto encontrado" }) 
 
     res.status(200).json(products)
   } catch (error) {
     res.status(500).json({ message: "Erro ao buscar produtos!" })
   }
-}  
+} 
